fadb06013da155c05d7bb1f84e238fc888c6c463
Sheevum Goel <126774066+sheevu@users.noreply.github.com>
2025-09-07 08:02:45 +0530

    Delete sudarshan-ai-labs directory

--- sudarshan-ai-labs/js/main.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('.leadform form').onsubmit = function(e){
-  e.preventDefault();
-  alert('Thank you! We will contact you soon.');
-  // You can add integration to Google Sheets or CRM here
-};
