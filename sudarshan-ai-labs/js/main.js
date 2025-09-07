bfc6611c7a3174cf82a0bb68c650cc01efbb5c21
Sheevum Goel <126774066+sheevu@users.noreply.github.com>
2025-09-07 06:38:20 +0530

    Create main.js

---
+++ sudarshan-ai-labs/js/main.js
@@ -0,0 +1,5 @@
+document.querySelector('.leadform form').onsubmit = function(e){
+  e.preventDefault();
+  alert('Thank you! We will contact you soon.');
+  // You can add integration to Google Sheets or CRM here
+};
